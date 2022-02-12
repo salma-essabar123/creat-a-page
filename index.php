f5cce4a578359d8b38d0b1bee5e164202a620dab
salma-essabar123 <96539285+salma-essabar123@users.noreply.github.com>
2022-02-12 16:34:55 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>LOGIN</title>
+</head>
+<body >  
+    <div class="f1">
+    <h1>LOGIN SYSTEM</h1>
+        <h3>account</h3>
+        <form action="login.php" method="POST">
+            <input type="text" placeholder="type ur acount" name="user">
+            <h3>password</h3>   
+            <input type="password" placeholder="type ur password" name="pass"> <br> <br>
+            <input type="submit" name="sub2" value="SING IN" >
+            </form>
+        <form action="login2.php" method="POST">
+        <input type="submit" name="sub" value="SING UP">  
+            </form>
+    </div>
+    
+</body>
+</html>
